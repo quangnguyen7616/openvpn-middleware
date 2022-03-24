9f416f55ace6305d8d5e1f0e3646cd2b41a38e3a
Quang Nguyen <quang.nguyen@speedup.vn>
2022-03-24 16:23:20 +0700

    update

--- middleware.js
+++ middleware.js
@@ -1,15 +1,18 @@
 const exec = require('child_process').exec;
 run = async () => {
     const openvpn = await exec(`openvpn --client --dev tun --proto tcp --remote th-bkk.prod.surfshark.com 1443 --resolv-retry infinite --remote-random --nobind --tun-mtu 1500 --tun-mtu-extra 32 --mssfix 1450 --persist-key --persist-tun --ping 15 --ping-restart 0 --ping-timer-rem --reneg-sec 0 --remote-cert-tls server --auth-user-pass login.info --verb 3 --pull --fast-io --cipher AES-256-CBC --auth SHA512 --ca cert.ca --key-direction 1 --tls-auth tls.key`);
+    let connected = false;
     openvpn.stdout.on('data', (data) => {
-        if (data.indexOf('Restart pause') != -1) {
-            console.error(data);
-        } else {
-            console.log(data);
+        if(data.indexOf('Initialization Sequence Completed') != -1){
+            connected = true;
         }
+        console.log(data);       
     });
     openvpn.stderr.on('data', (data) => {
         console.error(data);
     });
+    setTimeout(function(){
+        console.log('Connected', connected);
+    }, 30000);
 };
 run();
